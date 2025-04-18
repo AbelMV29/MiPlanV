beaf6847ddc58804c048dc924fe572468db57b90
abelmv <mvabel23@gmail.com>
2025-04-18 16:25:51 -0300

    Migración para image nullable y se solucionóerror en las fechas de la seed data

---
+++ MiPlanV.Infrastructure/Persistence/Migrations/20250418192301_ImageColumnsToStringNullableAndFixDateColumn.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiPlanV.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageColumnsToStringNullableAndFixDateColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "PackedLunches",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Quantities",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Quantities",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Quantities",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Sizes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Sizes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 18, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "PackedLunches",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3674));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3679));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3683));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3688));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3694));
+
+            migrationBuilder.UpdateData(
+                table: "Offers",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3697));
+
+            migrationBuilder.UpdateData(
+                table: "Quantities",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3579));
+
+            migrationBuilder.UpdateData(
+                table: "Quantities",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3584));
+
+            migrationBuilder.UpdateData(
+                table: "Quantities",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3587));
+
+            migrationBuilder.UpdateData(
+                table: "Sizes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3015));
+
+            migrationBuilder.UpdateData(
+                table: "Sizes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 4, 14, 15, 48, 32, 554, DateTimeKind.Utc).AddTicks(3019));
+        }
+    }
+}
